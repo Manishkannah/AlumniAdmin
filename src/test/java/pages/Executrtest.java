4dca5424272f8d09428f9c43afdad349af6b97ff
ManishKannah <manishkanna68@gmail.com>
2024-08-09 13:49:28 +0530

    executable code added

---
+++ src/test/java/pages/Executrtest.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.testng.xml.XmlSuite;
+
+public class Executrtest {
+//	public static void main(String[]args) {
+//		XmlSuite Suite = new XmlSuite();
+//		Suite.setName("suite");
+//	}
+
+}
